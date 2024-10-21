aaf2af4b92fd1d6be1fe1b0b1a6c35c1e757f992
prNikson <100094578+prNikson@users.noreply.github.com>
2024-10-21 18:07:54 +0300

    Update Nim.py

--- Nim.py
+++ Nim.py
@@ -4,26 +4,46 @@
 
 import random
 
+MIN_COUNT_OF_STONES = 1
+MAX_COUNT_OF_STONES = 9
+MIN_COUNT_OF_PILE = 2
+MAX_COUNT_OF_PILE = 5
+
 def main():
     # Define an empty rocklist to append rocks to, define random integers, call functions
     rockList = []
-    randPile = random.randint(2, 5)
-    randRock = random.randint(1, 9)
-    name1, name2 = get_players()
-
-    player = name1 # Set current player to 1 (for switching later)
+    randPile = random.randint(MIN_COUNT_OF_PILE, MAX_COUNT_OF_PILE)
+    randRock = random.randint(MIN_COUNT_OF_STONES, MAX_COUNT_OF_STONES)
+    players_tuple = get_players()
+    player = players_tuple[random.randint(0, 1)] # Set current player to 1 (for switching later)
+    name1, name2 = players_tuple
     
-    get_board(rockList, randPile, randRock, player) # Set initial board
-
+    get_board(rockList, randPile, randRock) # Set initial board
     play_again(rockList, randPile, randRock, name1, name2, player) 
 
 # Post: takes no arguments and returns the player names as strings entered by the user            
 def get_players():
-    return input("Enter player 1 name: "), input("Enter player 2 name: ")
+    while True:
+        name1 = input("Enter player 1 name: ")
+        if len(name1) < 3:
+            print("Very short nickname. Limitation for input - from 3 for 16 characters. Try again")
+        elif len(name1) > 10:
+            print("Very long nickname. Limitation for input - from 3 for 16 characters. Try again")
+        else:
+            break
+    while True:
+        name2 = input("Enter player 2 name: ")
+        if len(name2) < 3:
+            print("Very short nickname. Limitation for input - from 3 for 16 characters. Try again")
+        elif len(name2) > 10:
+            print("Very long nickname. Limitation for input - from 3 for 16 characters. Try again")
+        else:
+            break
+    return name1, name2
 
 # Pre : accepts empty rockList, random pile and rock as integers, current player as string
 # Post: modifies rockList with random piles and stones, prints initial starting board
-def get_board(rockList, randPile, randRock, player):
+def get_board(rockList, randPile, randRock):
 
     # Get initial board
     print("Let's look at the board now.")
@@ -44,23 +64,34 @@ def get_valid_input(rockList, randPile, player):
     # Begin loop that tests for valid input - if valid, break loop - if not, keep asking
     while True:
         stones = input('{}, how many stones to remove? '.format(player))
-        piles = input('Pick a pile to remove from: ')
+        if valid_stones_count(stones, rockList):
+            break
+        print(f"You can enter from {min(rockList)} to {max(rockList)} stones, and you have entered {stones}. Try again")
 
-        # If all condiitons for input are CORRECT, break out of loop
-        if (stones and piles) and (stones.isdigit()) and (piles.isdigit()):
-            if (int(stones) > 0) and (int(piles) <= len(rockList)) and (int(piles) > 0):
-                if (int(stones) <= rockList[int(piles) - 1]):
-                    if (int(stones) != 0) and (int(piles) != 0):
-                        break
-        
-        # If not, display this statement
-        print("Hmmm. You entered an invalid value. Try again, {}.".format(player))
-        
-    # Update state
+    while True:   
+        piles = input('Pick a pile to remove from: ')
+        if valid_piles_count(piles, stones, rockList):
+            break
+
     rockList[int(piles) - 1] -= int(stones)
+        
 
     # Keep playing game
     continue_game(rockList, randPile, player)
+
+def valid_stones_count(stones: int, rockList: list[int]) -> bool:
+    if stones and stones.isdigit() and int(stones) >= 1 and int(stones) <= max(rockList):
+        return True
+    return False
+
+def valid_piles_count(piles: int, stones: int, rockList: list[int]) -> bool:
+    if piles and piles.isdigit() and int(piles) >= 1 and int(piles) <= len(rockList):
+        if rockList[int(piles) - 1] - int(stones) < 0:
+            print(f"You can`t remove {stones} stones from {piles} pile. Try again")
+            return False
+        return True
+    print(f"You can enter from 1 to {len(rockList)} piles, and you entered {piles}. Try again")
+    return False
 
 # Pre : accepts modified rockList, random integer for piles, current player as string
 # Post: prints out updated game board after moves have been made, displays computer hint
@@ -89,7 +120,7 @@ def play_again(rockList, randPile, randRock, name1, name2, player):
         # To determine winner, check if rockList contains all 0's on that player's turn
         if rockList == [0] * len(rockList):
             print("{} is the winner of this round!".format(player))
-            user = input("Do you want to play again? Enter y for yes, anything for no: ")
+            user = input("Do you want to play again? Enter y for yes, any characters for no: ")
 
             if user.lower() == 'y':
                 # reset all conditions, start the game again
@@ -97,7 +128,7 @@ def play_again(rockList, randPile, randRock, name1, name2, player):
                 randPile = random.randint(2, 5)
                 name1, name2 = get_players()
                 player = name1
-                get_board(rockList, randPile, randRock, player)
+                get_board(rockList, randPile, randRock)
                 get_valid_input(rockList, randPile, player)
                 
             else:
@@ -150,4 +181,3 @@ def nim_sum(rockList, randPile):
         print("Pick all stones from pile {}.".format(rockList.index(max(rockList))+ 1 ))
 
 main()
-    
